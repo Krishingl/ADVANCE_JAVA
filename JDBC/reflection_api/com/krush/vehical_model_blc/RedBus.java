2776e5952876b0242c9c52cfc0c83c68f2bec4e6
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-08-11 10:02:04 +0530

    RedBus And Volvo sub type of Bus and Both Concrete class means Full Implementation.

---
+++ JDBC/reflection_api/com/krush/vehical_model_blc/RedBus.java
@@ -0,0 +1,12 @@
+package com.krush.vehical_model_blc;
+
+
+public class RedBus extends Bus{
+
+	@Override
+	public void engine() {
+	System.out.println("RedBus engine capacity is 40 KMPH");
+		
+	}
+
+}
---
+++ JDBC/reflection_api/com/krush/vehical_model_blc/Volvo.java
@@ -0,0 +1,16 @@
+package com.krush.vehical_model_blc;
+
+public class Volvo  extends Bus{
+
+	@Override
+	public void engine() {
+		System.out.println(" Volvo engine capacity 110 KMPH.");
+		
+	}
+	
+	@Override 
+	public void breaks() {
+		System.out.println("Volvo needs Powerfull breaks.");
+	}
+
+}
