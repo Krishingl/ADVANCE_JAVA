9160c2c4771ec09d9d2d4ff9cc392dbe04da0c73
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-08-18 17:50:13 +0530

    done

---
+++ JDBC/src/com/krush/jdbc_demo/Test01_Connection.java
@@ -0,0 +1,36 @@
+package com.krush.jdbc_demo;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Test01_Connection{
+	
+	public static void main(String[] args) {
+		   
+		
+		
+		try {
+			
+			//1.Loading  the Oracle Driver into JVM
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//2. Create the Connection to DataBase   //jdbc:oracle:thin:@//<host>:<port>/<service_name>
+
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","mydb9am","123");
+			
+			System.out.println("Connection Established");
+			//Printing the Connection Object Class Name
+			System.out.println(con);//oracle.jdbc.driver.T4CConnection@275710fc  [fQN +@+ hashCode()]
+			//Closing Connection
+			con.close();
+	
+		
+		}catch(ClassNotFoundException |SQLException e) {
+			e.printStackTrace();
+		}
+		
+	
+		
+	}
+
+}
